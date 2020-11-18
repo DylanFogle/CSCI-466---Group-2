20058fe34b285dd3b2b71ccd2e28c3ffb700f403
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-17 19:26:01 -0600

    Create addToMealDatabase.php

---
+++ addToMealDatabase.php
@@ -0,0 +1,4 @@
+<?php
+  // You must implement a page that facilitates the addition of new foods/drinks into the database.
+  // This should allow any of the relevant information (calories, macros, micronutrients, etc.) to be added.
+?>
