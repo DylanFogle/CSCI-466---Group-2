ebdcf576b874eca76610bddc5a5c3a7e19093db6
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-17 19:27:29 -0600

    Create enterMealData.php

---
+++ enterMealData.php
@@ -0,0 +1,5 @@
+<?php
+  // You must implement a page that allows the user to enter the foods/drinks they have consumed, and
+  // in what quantities (tracking page). This page must allow the amount consumed to be specified in any
+  // relevant type of unit, and the rest of your app should be able to handle those conversions.
+?>
