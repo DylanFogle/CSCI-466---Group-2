a9faf627fc480191ec88d7130421962e21c0deb8
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-17 19:28:36 -0600

    Create showFoodConsumption.php

---
+++ showFoodConsumption.php
@@ -0,0 +1,7 @@
+<?php
+  // You must implement a page that shows a table that lists of all the food consumed over a selected period
+  // of time, along with the calories and macros that are contained in the quantity of food specified. You
+  // must implement this page in such a way that clicking on the column headings will make your page sort
+  // based on that column. First click should sort in ascending order, a second should sort in descending
+  // order, and subsequent clicks just toggle that back and forth.
+?>
