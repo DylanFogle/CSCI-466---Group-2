4d5394e20536e86bae891e12e9b20aa457cc5d3e
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-17 19:31:11 -0600

    Create showWorkoutUsage.php

---
+++ showWorkoutUsage.php
@@ -0,0 +1,5 @@
+<?php
+  // You must implement a page showing workouts over a given time period, including estimated calories
+  // burned, and any other relevant information, for each workout. Statistics such as total calories and
+  // average calories per workout should be displayed on this page as well.
+?>
