55917cf4a4b141e8e5ba8717532e8f854deed113
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-23 10:16:57 -0600

    Update caloriesConsumedPerWeek.php

--- caloriesConsumedPerWeek.php
+++ caloriesConsumedPerWeek.php
@@ -1,3 +1,6 @@
 <?php
   // A graph will be generated of how many calories a user consumed each day of the week.
+
+  // Same idea as the calories burned in a week. Ask the user to give a date,
+  // calculate the week from that and give values for the whole week.
 ?>
