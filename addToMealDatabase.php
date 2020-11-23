5e7918f4e93eb97027199a895b8df767164c3de7
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-23 09:13:16 -0600

    Finished
    Finished, but can be adjusted to add more attributes and whatnot.

--- addToMealDatabase.php
+++ addToMealDatabase.php
@@ -1,4 +1,26 @@
 <?php
   // You must implement a page that facilitates the addition of new foods/drinks into the database.
   // This should allow any of the relevant information (calories, macros, micronutrients, etc.) to be added.
+
+  echo "Food/Drink Database Insertion!<br />";
+  echo "<form method=POST>";
+		echo "Input food/drink name here - <input type=text name=newFDName><br />";
+		echo "Input micronutrients per gram here - <input type=text name=newFDMicro><br />";
+		echo "Input macronutrients per serving here - <input type=text name=newFDMacro><br />";
+    // Can add more depending on what is needed.
+		echo "<input type=submit value='Submit to add food/drink!'/>";
+	echo "</form>";
+  if(!empty($_POST["newFDName"]) && !empty($_POST["newFDMicro"]) && !empty($_POST["newFDMacro"])){
+		$newFDName = $_POST["newFDName"];
+		$newFDMicro = $_POST["newFDMicro"];
+		$newFDMacro = $_POST["newFDMacro"];
+		$sql = "INSERT INTO DB_NAME VALUES (:Name,:Micro,:Macro);";
+		$prepared = $pdo->prepare($sql);
+		$success = $prepared->execute(array(":Name" => "$newFDName", ":Micro" => "$newFDMicro",
+			 ":Macro" => "$newFDMacro"));
+		if(!$success){
+			echo "Error in query";
+			die();
+		}
+	}
 ?>
