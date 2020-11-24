26158bf97e0353bf893299ef7ec2e999f455d0b9
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-24 10:24:55 -0600

    Finished?

--- updateWeight.php
+++ updateWeight.php
@@ -1,3 +1,23 @@
 <?php
   // You must implement a page that allows the user to update their weight.
+
+  echo "Update your weight!<br />";
+  echo "<form method=POST>";
+    echo "Enter your current weight, in pounds, here<input type=text name=newWeight><br />";
+    echo "Enter the date, in in the formation YYYY-MM-DD, here<input type=text name=weightDate><br />";
+    echo "<input type=submit value='Submite to log your weight!'>";
+  echo "</form>";
+
+  if(!empty($_POST["newWeight"]) && !empty($_POST["weightDate"])){
+    $newWeight = $_POST["newWeight"];
+    $weightDate = $_POST["weightDate"];
+    
+    $sql = "INSERT INTO Weight(WeightValue,Date) VALUES (:Weight,:Date);";
+    $prepared = $pdo->prepare($sql);
+    $success = $prepared->execute(array(":Weight" => "$newWeight", ":Weight" => "$weightDate"));
+    if(!$success){
+			echo "Error in query";
+			die();
+		}
+  }
 ?>
