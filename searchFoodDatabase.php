ca147fe91d105855221c535edd50210ea86e77b2
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-24 10:02:41 -0600

    Progress (Micronutrients)

--- searchFoodDatabase.php
+++ searchFoodDatabase.php
@@ -1,4 +1,37 @@
 <?php
   // Allow the user to search through the food database to find common foods, in order to plan their diets.
   // These same foods will be used to track their eating.
+
+  // Allow the user to select a food/drink from the database using a drop down menu.
+  // From here just display the information about the chosen item.
+
+  echo "Search the Food/Drink Database!<br />";
+  echo "<form method=POST>";
+  echo "Select a food/drink from the list<select name=pickedItem>";
+    $itemDataResult = $pdo->query("SELECT FROM Food/Drink;")
+		$itemDataRows = $itemDataResult->fetchAll(PDO::FETCH_ASSOC);
+		foreach($itemDataRows as $row){
+			echo "<option value=".$row["Name"].">".$row["Name"]."</option>";	
+		}
+  echo "</select><br />";
+  echo "<input type=submit value='Submit to see data about item!'>";
+  echo "</form>";
+
+  if(!empty($_POST["pickedItem"])){
+    $itemName = $_POST["pickedItem"];
+    $sql = "SELECT * FROM Food/Drink WHERE Name=:Name;";
+    $prepared = $pdo->prepare($sql);
+    $success = $prepared->execute(array(":Name" => "$itemName"));
+		if(!$success){
+			echo "Error in query";
+			die();
+		}
+    $rowsItem = $prepared->(PDO::FETCH_ASSOC);
+    
+    // From here we have the food/drink, and can simply show all the data associated with it.
+    
+    echo "<table border=1>";
+    echo "<tr><th></th>"
+    echo "</table>";
+  }
 ?>
