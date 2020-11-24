2f16b74c5348af94dfb7f940f6fffe420f7dd3f2
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-24 09:06:35 -0600

    Progress

--- addToMealDatabase.php
+++ addToMealDatabase.php
@@ -9,20 +9,23 @@
 		echo "Input macronutrients of fats per serving here - <input type=text name=newFDFats><br />";
 		echo "Input macronutrients of carbohydrates per serving here - <input type=text name=newFDCarbo><br />";
 		echo "Input macronutrients of protein per serving here - <input type=text name=newFDProtein><br />";
+		echo "Input the size of a serving, in grams or mL, here - <input type=text name=newFDSize><br />";
     // Can add more depending on what is needed.
 		echo "<input type=submit value='Submit to add food/drink!'/>";
 	echo "</form>";
   if(!empty($_POST["newFDName"]) && !empty($_POST["newFDMicro"]) && !empty($_POST["newFDFats"])
-		&& !empty($_POST["newFDCarbo"]) && !empty($_POST["newFDProtein"])){
+		&& !empty($_POST["newFDCarbo"]) && !empty($_POST["newFDProtein"]) && !empty($_POST["newFDSize"])){
 		$newFDName = $_POST["newFDName"];
 		$newFDMicro = $_POST["newFDMicro"];
 		$newFDFats = $_POST["newFDFats"];
 		$newFDCarbo = $_POST["newFDCarbo"];
 		$newFDProtein = $_POST["newFDProtein"];
-		$sql = "INSERT INTO Food/Drink VALUES (:Name,:Micro,:MacFats,:MacCarbo,:MacProtein);";
+	  	$newFDSize = $_POST["newFDSize"]
+		$sql = "INSERT INTO Food/Drink VALUES (:Name,:Micro,:MacFats,:MacCarbo,:MacProtein,:Size);";
 		$prepared = $pdo->prepare($sql);
 		$success = $prepared->execute(array(":Name" => "$newFDName", ":Micro" => "$newFDMicro",
-			 ":MacFats" => "$newFDFats", ":MacCarbo" => "$newFDCarbo", ":MacProtein" => "newFDProtein"));
+			 ":MacFats" => "$newFDFats", ":MacCarbo" => "$newFDCarbo", ":MacProtein" => "newFDProtein",
+			 ":Size" => "$newFDSize"));
 		if(!$success){
 			echo "Error in query";
 			die();
