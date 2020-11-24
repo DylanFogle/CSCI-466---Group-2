1eb32a4485190390f28108e1792e0c2c83e28348
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-24 10:10:20 -0600

    Finished?

--- searchWorkoutDatabase.php
+++ searchWorkoutDatabase.php
@@ -1,4 +1,38 @@
 <?php
   // The user will search through a list of workouts to find the one closest to the one they’re going to do,
   // in order to track the activity.
+
+  // Essentially the same as the searchFoodDatabase.php script. Show the user the available workouts,
+  // and when one is chosen, show the information on it.
+
+  echo "Search the Workout Database!<br />";
+  echo "<form method=POST>";
+  echo "Select a workout from the list<select name=pickedWorkout>";
+    $pickedWorkoutResult = $pdo->query("SELECT FROM WorkoutDatabase;")
+		$pickedWorkoutRows = $pickedWorkoutResult->fetchAll(PDO::FETCH_ASSOC);
+		foreach($pickedWorkoutRows as $row){
+			echo "<option value=".$row["Name"].">".$row["Name"]."</option>";	
+		}
+  echo "</select><br />";
+  echo "<input type=submit value='Submit to see data about workout!'>";
+  echo "</form>";
+
+  if(!empty($_POST["pickedWorkout"])){
+    $pickedWorkout = $_POST["pickedWorkout"];
+    $sql = "SELECT * FROM WorkoutDatabase WHERE Name=:Name;";
+    $prepared = $pdo->prepare($sql);
+    $success = $prepared->execute(array(":Name" => "$pickedWorkout"));
+		if(!$success){
+			echo "Error in query";
+			die();
+		}
+    $rowsWorkout = $prepared->(PDO::FETCH_ASSOC);
+    
+    // From here we have the workout, and can simply show all the data associated with it.
+    
+    echo "<table border=1>";
+    echo "<tr><th>Name</th><th>Type</th><th>Intensity</th></tr>";
+    echo "<tr><td>".$rowsWorkout["Name"]."</td><td>".$rowsWorkout["Type"]."</td><td>".$rowsWorkout["Intensity"]."</td></tr>";
+    echo "</table>";
+  }
 ?>
