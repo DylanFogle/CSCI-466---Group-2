a235f7ae918d142e34a6b2dc1d9b8ba46c920453
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-27 09:27:59 -0600

    Finished

--- addToMealDatabase.php
+++ addToMealDatabase.php
@@ -5,7 +5,6 @@
   echo "Food/Drink Database Insertion!<br />";
   echo "<form method=POST>";
 		// Micronutrients will be in milligrams, Macronutrients will be in grams.
-		// We'll convert later.
 		echo "Input food/drink name here - <input type=text name=newFDName><br />";
 		echo "Input micronutrients of Vitamin A per milligram here - <input type=text name=newFDVitA><br />";
 		echo "Input micronutrients of Vitamin C per milligram here - <input type=text name=newFDVitC><br />";
@@ -31,7 +30,7 @@
 		$newFDProtein = $_POST["newFDProtein"];
 	  	$newFDSize = $_POST["newFDSize"];
 	  	$newFDCal = $_POST["newFDCal"];
-		$sql = "INSERT INTO Food/Drink (Name,VitaminA,VitaminC,Calcium,Iron,Fats,Carbohydrates,Protein,Size,Calories) ";
+		$sql = "INSERT INTO NUTRITIONINFO (Name,VITAMIN_A,VITAMIN_C,CALCIUM,IRON,FATS,CARBS,PROTEIN,SERVING_SIZE,CALORIES) ";
 		$sql2 = "VALUES (:Name,:VitA,:VitC,:Calc,:Iron,:Fats,:Carbo,:Protein,:Size,:Cal);";
 		$sql = $sql.$sql2
 		$prepared = $pdo->prepare($sql);
