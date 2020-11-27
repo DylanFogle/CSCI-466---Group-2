4325d92a77b58d47338657e12118713353185504
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-27 09:39:22 -0600

    Finished (Check Intensity)

--- enterWorkoutData.php
+++ enterWorkoutData.php
@@ -4,37 +4,32 @@
   echo "<br />Entering Workout Data!<br />";
   echo "<form method=POST>";
 		echo "Select workout name here<select name=workoutName>";
-			$workoutDataResult = $pdo->query("SELECT FROM Workout;")
+			$workoutDataResult = $pdo->query("SELECT NAME FROM WORKOUTINFO;")
 			$worokutDataRows = $workoutDataResult->fetchAll(PDO::FETCH_ASSOC);
 			foreach($workoutDataRows as $row){
-				echo "<option value=".$row["Name"].">".$row["Name"]."</option>";	
+				echo "<option value=".$row["NAME"].">".$row["NAME"]."</option>";	
 			}
 		echo "<select/><br />";
-		echo "Input workout type here<input type=text name=workoutType><br />";
-		echo "Input workout intensity here<input type=text name=workoutIntensity><br />";
+		//*echo "Input workout intensity here<input type=text name=workoutIntensity><br />";
 		echo "Input workout duration, in minutes, here<input type=text name=workoutDuration><br />";
 		echo "Input the date here, formatted as YYYY-MM-DD<input type=text name=workoutDate><br />";
 		echo "<input type=submit value='Submit to add workout!'/>";
   echo "</form>";
 
-	if(!empty($_POST["workoutName"]) && !empty($_POST["workoutType"]) && !empty($_POST["workoutIntensity"]) 
+	if(!empty($_POST["workoutName"]) && !empty($_POST["workoutIntensity"]) 
 		&& !empty($_POST["workoutDuration"]) && !empty($_POST["workoutDate"]){
 		$workoutName = $_POST["workoutName"];
-		$workoutType = $_POST["workoutType"];
-		$workoutIntensity = $_POST["workoutIntensity"];
+		//*$workoutIntensity = $_POST["workoutIntensity"];
 		$workoutDuration = $_POST["workoutDuration"];
 		$workoutDate = $_POST["workoutDate"];
-		$workoutCalories = 0;
-			
-		// Calculate how many calories were burned given workout and duration.
 		
-		$sql0 = "INSERT INTO Workout(Name,Type,Intensity,Duration,Calories,Date) ";
-      		$sql1 = "VALUES (:wName,:wType,:wIntensity,:wDuration,:wCalories,:wDate);";
+		//$sql0 = "INSERT INTO WORKOUT(NAME,INTENSITY,DURATION,DATE) ";
+		$sql0 = "INSERT INTO WORKOUT(NAME,DURATION,DATE) ";
+      		$sql1 = "VALUES (:wName,:wDuration,:wDate);";
       		$sql = $sql0.$sql;
 		$prepared = $pdo->prepare($sql);
-		$success = $prepared->execute(array(":wName" => "$workoutName", ":wType" => "$workoutType",
-			":wIntensity" => "$workoutIntensity", ":wDuration" => "$workoutDuration",
-       			":wCalories" => "$workoutCalories", ":wDate" => "$workoutDate"));
+		$success = $prepared->execute(array(":wName" => "$workoutName", ":wDuration" => "$workoutDuration",
+       			":wDate" => "$workoutDate"));
 		if(!$success){
 			echo "Error in query";
 			die();
