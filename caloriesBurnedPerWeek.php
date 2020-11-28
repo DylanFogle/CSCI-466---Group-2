e7d3d183e21c55afd2730fa204075a0e5f1d3106
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-28 14:52:07 -0600

    Finished

--- caloriesBurnedPerWeek.php
+++ caloriesBurnedPerWeek.php
@@ -25,14 +25,41 @@
 		}
     $rowsDoW = $prepared->fetchAll(PDO::FETCH_ASSOC);
     // We now have every distinct day of possible workout given by the user
+	$CBDay0 = "N/A";
+    $CBDay1 = "N/A";
+    $CBDay2 = "N/A";
+    $CBDay3 = "N/A";
+    $CBDay4 = "N/A";
+    $CBDay5 = "N/A";
+    $CBDay6 = "N/A"; 
     $i = 0;
     foreach($rowsDoW as $row){
-      $CBDay.$i = $row["DATE"];
-      $i = $i + 1;
+      if($i == 0){
+                $CBDay0 = $row["DATE"];
+        }
+        if($i == 1){
+                $CBDay1 = $row["DATE"];
+        }
+        if($i == 2){
+                $CBDay2 = $row["DATE"];
+        }
+        if($i == 3){
+                $CBDay3 = $row["DATE"];
+        }
+        if($i == 4){
+                $CBDay4 = $row["DATE"];
+        }
+        if($i == 5){
+                $CBDay5 = $row["DATE"];
+        }
+        if($i == 6){
+                $CBDay6 = $row["DATE"];
+        }
+      $i++;
     }
     // But we need to redo the query again to get all workouts done in that time.
     $sql1 = "SELECT NAME,DURATION,DATE FROM WORKOUT WHERE DATE >= :CBFD AND DATE <= :CBLD;";
-    $prepared1 = $pdo->prepare($sql);
+    $prepared1 = $pdo->prepare($sql1);
     $success1 = $prepared1->execute(array(":CBFD" => "$CBFirstDate", ":CBLD" => "$CBLastDate"));
 		if(!$success1){
 			echo "Error in query";
@@ -57,25 +84,25 @@
         // We have the same workout.
         if($rowWOW["NAME"] == $rowWI["NAME"]){
           // Now check for day.
-          if($CBDay."0" == $rowWOW["DATE"]){
+          if($CBDay0 == $rowWOW["DATE"]){
             $CBDay0Amount += $rowWOW["DURATION"] * $rowWI["CALORIES_BURNED_PER_MINUTE"];
           }
-          if($CBDay."1" == $rowWOW["DATE"]){
+          if($CBDay1 == $rowWOW["DATE"]){
             $CBDay1Amount += $rowWOW["DURATION"] * $rowWI["CALORIES_BURNED_PER_MINUTE"];
           }
-          if($CBDay."2" == $rowWOW["DATE"]){
+          if($CBDay2 == $rowWOW["DATE"]){
             $CBDay2Amount += $rowWOW["DURATION"] * $rowWI["CALORIES_BURNED_PER_MINUTE"];
           }
-          if($CBDay."3" == $rowWOW["DATE"]){
+          if($CBDay3 == $rowWOW["DATE"]){
             $CBDay3Amount += $rowWOW["DURATION"] * $rowWI["CALORIES_BURNED_PER_MINUTE"];
           }
-          if($CBDay."4" == $rowWOW["DATE"]){
+          if($CBDay4 == $rowWOW["DATE"]){
             $CBDay4Amount += $rowWOW["DURATION"] * $rowWI["CALORIES_BURNED_PER_MINUTE"];
           }
-          if($CBDay."5" == $rowWOW["DATE"]){
+          if($CBDay5 == $rowWOW["DATE"]){
             $CBDay5Amount += $rowWOW["DURATION"] * $rowWI["CALORIES_BURNED_PER_MINUTE"];
           }
-          if($CBDay."6" == $rowWOW["DATE"]){
+          if($CBDay6 == $rowWOW["DATE"]){
             $CBDay6Amount += $rowWOW["DURATION"] * $rowWI["CALORIES_BURNED_PER_MINUTE"];
           }
         }  
@@ -83,9 +110,10 @@
     }
     
     echo "<table border=1>";
-    echo "<tr><th>".$CBDay."0"."</th><th>".$CBDay."1"."</th><th>".$CBDay."2"."</th>";
-    echo "<th>".$CBDay."3"."</th><th>".$CBDay."4"."</th><th>".$CBDay."5"."</th><th>".$CBDay."6"."</th></tr>";
-    echo "<tr><td>".$CBDay0Amount."</td><td>".$CBDay1Amount."</td><td>".$CBDay2Amount."</td><td>".$CBDay3Amount."</td>";
-    echo "<tr><td>".$CBDay4Amount."</td><td>".$CBDay5Amount."</td><td>".$CBDay6Amount."</td></tr>";
+    echo "<tr><th>$CBDay0</th><th>$CBDay1</th><th>$CBDay2</th>";
+    echo "<th>$CBDay3</th><th>$CBDay4</th><th>$CBDay5</th><th>$CBDay6</th></tr>";
+    echo "<tr><td>$CBDay0Amount</td><td>$CBDay1Amount</td><td>$CBDay2Amount</td><td>$CBDay3Amount</td>";
+    echo "<td>$CBDay4Amount</td><td>$CBDay5Amount</td><td>$CBDay6Amount</td></tr>";
+	echo "</table>";
   }
 ?>
