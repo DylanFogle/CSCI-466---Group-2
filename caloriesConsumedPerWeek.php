23fbe95b1e74a80d5176263bba7d5f9bb959ffc7
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-28 14:28:40 -0600

    Finished

--- caloriesConsumedPerWeek.php
+++ caloriesConsumedPerWeek.php
@@ -25,9 +25,36 @@
 		}
     $rowsDoW = $prepared->fetchAll(PDO::FETCH_ASSOC);
     // We now have every distinct day of possible meal eaten by the user
+	$CCDay0 = "N/A";
+    $CCDay1 = "N/A";
+    $CCDay2 = "N/A";
+    $CCDay3 = "N/A";
+    $CCDay4 = "N/A";
+    $CCDay5 = "N/A";
+    $CCDay6 = "N/A";
     $i = 0;
     foreach($rowsDoW as $row){
-      $CBDay.$i = $row["DATE"];
+       if($i = 0){
+                $CCDay0 = $row["DATE"];
+        }
+        if($i = 1){
+                $CCDay1 = $row["DATE"];
+        }
+        if($i = 2){
+                $CCDay2 = $row["DATE"];
+        }
+        if($i = 3){
+                $CCDay3 = $row["DATE"];
+        }
+        if($i = 4){
+                $CCDay4 = $row["DATE"];
+        }
+        if($i = 5){
+                $CCDay5 = $row["DATE"];
+        }
+        if($i = 6){
+                $CCDay6 = $row["DATE"];
+        }
       $i = $i + 1;
     }
     // But we need to redo the query again to get all meals eaten in that time.
@@ -41,42 +68,43 @@
     $rowsMoW = $prepared1->fetchAll(PDO::FETCH_ASSOC);
     // Because we already calculated the calories prior to insertion into FOOD_AND_DRINK,
     // we can simply tally them up day by day.
-    $CBDay0Amount = 0;
-    $CBDay1Amount = 0;
-    $CBDay2Amount = 0;
-    $CBDay3Amount = 0;
-    $CBDay4Amount = 0;
-    $CBDay5Amount = 0;
-    $CBDay6Amount = 0;
+    $CCDay0Amount = 0;
+    $CCDay1Amount = 0;
+    $CCDay2Amount = 0;
+    $CCDay3Amount = 0;
+    $CCDay4Amount = 0;
+    $CCDay5Amount = 0;
+    $CCDay6Amount = 0;
     // For each meal eaten by the user.
     foreach($rowsMoW as $rowMOW){
       // Now check for day.
-      if($CCDay."0" == $rowMOW["DATE"]){
+      if($CCDay0 == $rowMOW["DATE"]){
         $CCDay0Amount += $rowMOW["CALORIES"];
       }
-      if($CCDay."1" == $rowMOW["DATE"]){
+      if($CCDay1 == $rowMOW["DATE"]){
         $CCDay1Amount += $rowMOW["CALORIES"];
       }
-      if($CCDay."2" == $rowMOW["DATE"]){
+      if($CCDay2 == $rowMOW["DATE"]){
         $CCDay2Amount += $rowMOW["CALORIES"];
       }
-      if($CCDay."3" == $rowMOW["DATE"]){
+      if($CCDay3 == $rowMOW["DATE"]){
         $CCDay3Amount += $rowMOW["CALORIES"];
       }
-      if($CCDay."4" == $rowMOW["DATE"]){
+      if($CCDay4 == $rowMOW["DATE"]){
         $CCDay4Amount += $rowMOW["CALORIES"];
       }
-      if($CCDay."5" == $rowMOW["DATE"]){
+      if($CCDay5 == $rowMOW["DATE"]){
         $CCDay5Amount += $rowMOW["CALORIES"];
       }
-      if($CCDay."6" == $rowMOW["DATE"]){
+      if($CCDay6 == $rowMOW["DATE"]){
         $CCDay6Amount += $rowMOW["CALORIES"];
       }
     }
     echo "<table border=1>";
-    echo "<tr><th>".$CCDay."0"."</th><th>".$CCDay."1"."</th><th>".$CCDay."2"."</th>";
-    echo "<th>".$CCDay."3"."</th><th>".$CCDay."4"."</th><th>".$CCDay."5"."</th><th>".$CCDay."6"."</th></tr>";
-    echo "<tr><td>".$CCDay0Amount."</td><td>".$CCDay1Amount."</td><td>".$CCDay2Amount."</td><td>".$CCDay3Amount."</td>";
-    echo "<tr><td>".$CCDay4Amount."</td><td>".$CCDay5Amount."</td><td>".$CCDay6Amount."</td></tr>";
+    echo "<tr><th>$CCDay0</th><th>$CCDay1</th><th>$CCDay2</th>";
+    echo "<th>$CCDay3</th><th>$CCDay4</th><th>$CCDay5</th><th>$CCDay6</th></tr>";
+    echo "<tr><td>$CCDay0Amount</td><td>$CCDay1Amount</td><td>$CCDay2Amount</td><td>$CCDay3Amount</td>";
+    echo "<td>$CCDay4Amount</td><td>$CCDay5Amount</td><td>$CCDay6Amount</td></tr>";
+	echo "</table>";
   }
 ?>
