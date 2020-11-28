41246c71b91c2bd50a64bc9d0898a4ac18bf181b
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-28 11:35:47 -0600

    Finished

--- addToMealDatabase.php
+++ addToMealDatabase.php
@@ -2,7 +2,7 @@
   // You must implement a page that facilitates the addition of new foods/drinks into the database.
   // This should allow any of the relevant information (calories, macros, micronutrients, etc.) to be added.
 
-  echo "Food/Drink Database Insertion!<br />";
+  echo "<h4>Food/Drink Database Insertion!</h4>";
   echo "<form method=POST>";
 		// Micronutrients will be in milligrams, Macronutrients will be in grams.
 		echo "Input food/drink name here - <input type=text name=newFDName><br />";
