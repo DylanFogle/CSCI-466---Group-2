092786a43654b7ea7c047a350d860bf74e4d0b86
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-28 13:03:57 -0600

    Finished

--- userWeightOverTime.php
+++ userWeightOverTime.php
@@ -4,7 +4,7 @@
   // We can just select all the values from the Weight table and sort by date.
   echo "<h4>User weight over time!</h4>";
   $resultWOT = $pdo->query("SELECT DATE,NUMERIC_WEIGHT FROM WEIGHT ORDER BY DATE;");
-  $rowsWOT = $resultsWOT->fetchAll(PDO::FETCH_ASSOC);
+  $rowsWOT = $resultWOT->fetchAll(PDO::FETCH_ASSOC);
   echo "<table border=1>";
   echo "<tr><th>Date</th><th>Weight(lbs)</th></tr>";
   foreach($rowsWOT as $row){
