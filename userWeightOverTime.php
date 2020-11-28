186d5c559a861057d761d9c812cad469b7712ec3
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2020-11-28 11:46:37 -0600

    Finished

--- userWeightOverTime.php
+++ userWeightOverTime.php
@@ -2,7 +2,7 @@
   // A line graph of user weight over time. Can be represented just as a table.
 
   // We can just select all the values from the Weight table and sort by date.
-  echo "<br />User weight over time!<br />";
+  echo "<h4>User weight over time!</h4>";
   $resultWOT = $pdo->query("SELECT DATE,NUMERIC_WEIGHT FROM WEIGHT ORDER BY DATE;");
   $rowsWOT = $resultsWOT->fetchAll(PDO::FETCH_ASSOC);
   echo "<table border=1>";
